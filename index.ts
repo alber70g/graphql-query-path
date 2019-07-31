80c52ba2a2870600ea91f52774d3ea332d4c71f8
Albert Groothedde <albertgroothedde@gmail.com>
2019-07-31 23:43:42 +0200

    tests in progress

--- index.ts
+++ index.ts
@@ -5,42 +5,7 @@ import {
   SelectionNode
 } from "graphql";
 
-const ast = parse(`query { user(id: 1) { name posts { title content } } }`);
-
-/**
- * type Query {
- *  user: User
- * }
- * 
- * type User {
- *  name: String
- *  posts: [Post]
- * }
- * 
- * type Post {
- *  title: String
- *  content: String
- *  history: [Post]
- * }
- * 
- * N+1
- * 
- * /user/
- * /user/name
- * /user/posts/
- * /user/posts/title
- * /user/posts/content
- * 
- * 
- * resolver(parent, args, _, info) {
- *   
- *      db.usersWithPostsAndTags()
- *   }
- * }
- * 
- */
-
-const getPaths = (ast: DocumentNode) => {
+export const getPaths = (ast: DocumentNode) => {
   return ast.definitions.map(walkDefinitions)
 }
 
@@ -50,41 +15,26 @@ const walkDefinitions = (node: DefinitionNode) => {
       createReduceSelections("/"),
       []
     );
+  } else { 
+    return [];
   }
 }
 
-const createReduceSelections = (parent) => (
+const createReduceSelections = (parent: string) => (
   acc: string[],
   curr: SelectionNode
-) => {
+): string[] => {
   if (curr.kind === "Field") {
     if (curr.selectionSet && curr.selectionSet.selections) {
       acc.push(parent + curr.name.value + "/");
       return curr.selectionSet.selections.reduce(
         createReduceSelections(parent + curr.name.value + "/"),
-        [...acc]
+        acc
       )
     } else {
       acc.push(parent + curr.name.value);
       return acc;
     }
-  }
+  } 
+  return [];
 }
-
-const paths = getPaths(ast)
-
-console.log(paths);
-
-console.log(paths[0].find((p: string) => p.indexOf("user/posts") > -1));
-
-// const resolver = () => {
-//   if (paths[0].find((p: string) => p.indexOf("user/posts") > -1)) {
-//     const query = db.getUsers().join('posts');
-//     if (paths.contains('user/posts/history')) {
-//       query.on('posts').join('history');
-//     }
-//   } else {
-//     db.getUsers()
-//   }
-//   return query.execute();
-// }
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "roots": [
+      "<rootDir>"
+    ],
+    "transform": {
+      "^.+\\.tsx?$": "ts-jest"
+    },
+    "moduleDirectories": ["node_modules"]
+  }
+  
